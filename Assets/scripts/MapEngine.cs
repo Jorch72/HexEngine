e0278c437b0a0de50d0bd54c758d259248fa2d95
perkanus <pkarolidis@yahoo.com>
2018-09-19 10:58:33 +0200

    Initial
    Initial Version

---
+++ Assets/scripts/MapEngine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapEngine: MonoBehaviour
+{
+    public GameObject hexPrefab;
+    bool flat_topped = true;
+
+    Vector3[] corners = new Vector3[6];
+    float width;
+    float height;
+
+   
+    void Start()
+    {
+        int mapwidth = 32;
+        int mapheight = 32;
+        float size = 0.88f;
+
+        width = 2 * size;
+        height = Mathf.Sqrt(3) * size;
+          
+        Vector3 center = new Vector3(0, 0, 0);
+        for (int i = 1; i < mapwidth; i++)
+        {
+            for (int j = 0; j < mapheight; j++)
+            {
+
+
+                GameObject hx_go = (GameObject) Instantiate(hexPrefab, center, Quaternion.identity);
+               
+                hx_go.name = "Position_" + i + "_" + j;
+                hx_go.transform.SetParent(this.transform);
+                hx_go.isStatic = true;
+          
+                center.x = center.x + width;
+                
+               
+            }
+            center.z = center.z + height;
+
+
+            if (i % 2 == 0)
+            {
+                Debug.Log(i % 2);
+                center.x = 0;
+            }           
+            else
+            {
+                Debug.Log(i % 2);
+                center.x = width / 2;
+            }
+                
+        }
+
+        }
+
+        
+
+        
+
+        
+
+}
---
+++ Assets/scripts/MouseManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log("Mouseposition: " + Input.mousePosition);
+
+        //
+        //GameObject.Find("Camera Bob").GetComponent<Camera>();
+        // In orthographic mode
+        //Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log("World Point: " + worldPoint);
+        RaycastHit hitInfo;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            //Debug.Log("Raycast hit:" + hitInfo.collider.gameObject.name);
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Hej");
+            }
+        }
+        
+
+
+    }
+}
